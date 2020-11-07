cddcd1d28805a4eeb416e1b9f2c004f3bcd19289
artyom <art.volyk@gmail.com>
2020-11-07 02:41:40 +0200

    download books with titles

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-os.makedirs('books', exist_ok= True)
-os.chdir('books')
-for id in range(1, 11):
-    url = f'https://tululu.org/txt.php?id={id}'
+
+def get_title(id):
+    url = f'https://tululu.org/b{id}/'
     response = requests.get(url)
     response.raise_for_status()
     if response.url == url:
-        with open(f'text{id}.txt', 'wb') as file:
+        soup = BeautifulSoup(response.text, features="lxml")
+        title = soup.find('h1').text
+        title = title.split('::')[0].strip()
+        author = soup.find('h1').find('a').text.strip()
+        return {'title': title, 'author': author}
+    return
+
+
+
+def download_txt(url, filename, folder='books/'):
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+    if response.url == url:
+        with open(filepath, 'wb') as file:
             file.write(response.content)
+
+
+for book_id in range(1, 10):
+    try:
+        title = get_title(book_id)['title']
+    except TypeError:
+        continue
+    url = f'https://tululu.org/txt.php?id={book_id}'
+    download_txt(url, title, folder='books/')
+
+
 
