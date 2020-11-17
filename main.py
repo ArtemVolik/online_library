8d42eb981beafc1212f026091d79313938db6dec
artyom <art.volyk@gmail.com>
2020-11-17 12:07:05 +0200

    Revert "flatten code"
    This reverts commit 592c6c98adffb8588a0e6192d9d558b0a74894f3.

--- main.py
+++ main.py
@@ -62,61 +62,60 @@ def get_book_info(book_url, skip_image, skip_txt, images_folder, text_folder, js
     response = requests.get(url)
     response.raise_for_status()
 
-    if not response.url == url:
-        return
-    soup = BeautifulSoup(response.text, features="lxml")
+    if response.url == url:
+        soup = BeautifulSoup(response.text, features="lxml")
 
-    book_title = soup.select_one('h1').text
-    book_title = book_title.split('::')[0].strip()
+        book_title = soup.select_one('h1').text
+        book_title = book_title.split('::')[0].strip()
 
-    author_selector = 'h1 a'
-    book_author = soup.select_one(author_selector).text.strip()
+        author_selector = 'h1 a'
+        book_author = soup.select_one(author_selector).text.strip()
 
-    image_selector = ' .bookimage img'
-    image_src = soup.select_one(image_selector)['src']
+        image_selector = ' .bookimage img'
+        image_src = soup.select_one(image_selector)['src']
 
-    scheme, path = urlparse(url)[0:2]
-    image_url = urljoin(f'{scheme}://{path}', image_src)
-    image_extension = image_src.split('/')[2]
+        scheme, path = urlparse(url)[0:2]
+        image_url = urljoin(f'{scheme}://{path}', image_src)
+        image_extension = image_src.split('/')[2]
 
-    image_path = ''
-    if not skip_image:
-        image_path = download_image(image_url, image_extension, images_folder)
+        image_path = ''
+        if not skip_image:
+            image_path = download_image(image_url, image_extension, images_folder)
 
-    book_comments_selector = '.texts > .black'
-    book_comments = soup.select(book_comments_selector)
-    book_comments = [comment.text for comment in book_comments]
+        book_comments_selector = '.texts > .black'
+        book_comments = soup.select(book_comments_selector)
+        book_comments = [comment.text for comment in book_comments]
 
-    genres_selector = 'span.d_book a'
-    genres = soup.select(genres_selector)
-    genres = [genre.text for genre in genres]
+        genres_selector = 'span.d_book a'
+        genres = soup.select(genres_selector)
+        genres = [genre.text for genre in genres]
 
-    # без этой проверки будет ошибка при поиске ссылки на файл для скачивания со страницы книжки, если
-    # ссылки на скачивание на странице нет
-    try:
-        # не понял как селектом заменить, подскажите
-        book_url_href = soup.find('table', class_='d_book').find('a', title=re.compile(r'txt'))['href']
+        # без этой проверки будет ошибка при поиске ссылки на файл для скачивания со страницы книжки, если
+        # ссылки на скачивание на странице нет
+        try:
+            # не понял как селектом заменить, подскажите
+            book_url_href = soup.find('table', class_='d_book').find('a', title=re.compile(r'txt'))['href']
             # print(url)
-    except BaseException:
-        return
+        except BaseException:
+            return
 
-    book_txt_download_url = urljoin(f'{scheme}://{path}', book_url_href)
-    book_path = ''
-    if not skip_txt:
-        book_path = download_txt(book_txt_download_url, book_title, text_folder)
+        book_txt_download_url = urljoin(f'{scheme}://{path}', book_url_href)
+        book_path = ''
+        if not skip_txt:
+            book_path = download_txt(book_txt_download_url, book_title, text_folder)
 
-    books_info.append(
-        {
-            'title': book_title,
-            'author': book_author,
-            'image_src': image_path,
-            'book_path': book_path,
-            'comments': book_comments,
-            'genres': genres
-        })
+        books_info.append(
+            {
+                'title': book_title,
+                'author': book_author,
+                'image_src': image_path,
+                'book_path': book_path,
+                'comments': book_comments,
+                'genres': genres
+            })
 
-    with open(json_path, 'w', encoding='utf8') as file:
-        json.dump(books_info, file, ensure_ascii=False)
+        with open(json_path, 'w', encoding='utf8') as file:
+            json.dump(books_info, file, ensure_ascii=False)
 
 
 def download_txt(url, filename, folder='books/'):
@@ -124,11 +123,10 @@ def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
     response.raise_for_status()
     filepath = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
-    if not response.url == url:
-        return
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
-        return filepath
+    if response.url == url:
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
+            return filepath
 
 
 def download_image(url, filename, folder='images/'):
@@ -136,11 +134,10 @@ def download_image(url, filename, folder='images/'):
     response = requests.get(url)
     response.raise_for_status()
     filepath = os.path.join(folder, filename)
-    if not response.url == url:
-        return
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
-        return filepath
+    if response.url == url:
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
+            return filepath
 
 
 if __name__ == '__main__':
